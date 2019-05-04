794ac9bd6aa08bb3b32f552fa4adb7ee3236c3c5
itisha2987 <Itishapatidar1@gmail.com>
2019-05-04 20:18:38 +0530

    Fisrt commit

--- src/main/java/com/example/complaintmanagementsystem/WriteComplaint.java
+++ src/main/java/com/example/complaintmanagementsystem/WriteComplaint.java
@@ -1,5 +1,6 @@
 package com.example.complaintmanagementsystem;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -11,15 +12,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.MultiAutoCompleteTextView;
 
-import com.example.complaintmanagementsystem.Models.Complaint.Complaint;
-import com.example.complaintmanagementsystem.utils.Helper;
+
+import com.example.complaintmanagementsystem.MainHelpers.GMailSender;
+
 
 import org.w3c.dom.Text;
 
 public class WriteComplaint extends AppCompatActivity {
     private static String complaintType;
-    private static Button submit;
+    private static Button btnSend;
     private static String complaintDetails;
+    EditText etContent, etRecipient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,35 +33,46 @@ public class WriteComplaint extends AppCompatActivity {
         complaintType = intent.getExtras().getString("complaintType");
 
 
+        etContent = (EditText) findViewById(R.id.etContent);
+        etRecipient = (EditText)findViewById(R.id.etRecipient);
 
-
-        submit = findViewById(R.id.submit);
-        submit.setOnClickListener(new View.OnClickListener() {
+        btnSend = (Button) findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                submitComplaint();
+                sendMessage();
             }
         });
     }
 
     private void submitComplaint(){
-        TextInputLayout complaintDetailsText= findViewById(R.id.complaintDetails);
-        complaintDetails = complaintDetailsText.getEditText().getText().toString();
 
-        if(complaintDetails.isEmpty()){
-            complaintDetailsText.setError("Field cannot be empty");
-        }
-        else if(complaintDetails.length() < 20){
-            complaintDetailsText.setError("Minimum 20 characters");
-        }
-        else if(complaintDetails.length() > 1000) {
-            complaintDetailsText.setError("Maximum 30 characters");
-        }
-        Complaint newComplaint = new Complaint(complaintType, complaintDetails);
-        Log.e("s:", "SubmitComplaint");
-        Log.e("Cab:", complaintDetails);
-        Log.e("Type", complaintType);
-        Log.e("status","Pending");
-        Log.e("date", newComplaint.getDate());
+    }
+
+    private void sendMessage() {
+
+        final ProgressDialog dialog = new ProgressDialog(WriteComplaint.this);
+        dialog.setTitle("Sending Email");
+        dialog.setMessage("Please wait");
+        dialog.show();
+        Thread sender = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    GMailSender sender = new GMailSender("cmsiitj2019@gmail.com", "cms@cs223");
+                    sender.sendMail("EmailSender App",
+                            etContent.getText().toString(),
+                            "youremailaddress",
+                            etRecipient.getText().toString());
+                    dialog.dismiss();
+                } catch (Exception e) {
+                    Log.e("mylog", "Error: " + e.getMessage());
+                }
+            }
+        });
+        sender.start();
     }
 }
+
+
+
